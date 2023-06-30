9bec673a9f44ee6eaaa7ba41836e7af8fea09728
Caterina Penone <caterinap@users.noreply.github.com>
2023-06-30 10:37:04 +0200

    Create Next_Update.R

---
+++ Next_Update.R
@@ -0,0 +1,8 @@
+##Replace symbionts with proper AMF datasets, see grassland dataset
+
+
+###Birds 2018 --> change dataset ID and version
+#From Cornelia: to make the dataset 25306 "Bird survey and trait data on all grassland and forest EPs 2018" public, 
+#we had to upload the data in a new dataset. This dataset (31521) contains abbreviations for the observer. All other 
+#data is identical. Dataset 25306 was deleted. I updated the link section of your dataset - I deleted the link to 25306 
+#and added a link to  31521 in your synthesis dataset (27706, 27707, 31206) and dataset 31368."
