8bed57b4a67a299273a2cd3fed67e58f3d48756f
Caterina Penone <caterinap@users.noreply.github.com>
2023-08-24 10:59:36 +0200

    Update Next_Update.R

--- Next_Update.R
+++ Next_Update.R
@@ -6,3 +6,5 @@
 #we had to upload the data in a new dataset. This dataset (31521) contains abbreviations for the observer. All other 
 #data is identical. Dataset 25306 was deleted. I updated the link section of your dataset - I deleted the link to 25306 
 #and added a link to  31521 in your synthesis dataset (27706, 27707, 31206) and dataset 31368."
+
+#HEW51 has zeros (and not NAs) for birds before 2017! check this for all groups and change
